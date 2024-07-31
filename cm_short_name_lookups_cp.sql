352e2319ac28d6e29f22b6dd563ebb5827199f17
prakashvel-m <123497509+prakashvel-m@users.noreply.github.com>
2024-07-31 11:28:23 +0530

    Create cm_short_name_lookups_cp.sql

---
+++ cm_short_name_lookups_cp.sql
@@ -0,0 +1,33 @@
+select
+concurrent_program_name,user_concurrent_program_name
+from 
+fnd_concurrent_programs_vl b 
+where
+user_concurrent_program_name like '%GEPO Agile EBS Item Interface Request Set - APL%';
+
+select
+c.program_short_name,c.program,b.user_concurrent_program_name
+from 
+fnd_concurrent_programs_vl b,FND_CONC_REQ_SUMMARY_V c
+where
+c.program_short_name=b.concurrent_program_name
+and b.user_concurrent_program_name like '%GEONT EDI 860 Order Update - APL%';
+
+select 
+a.lookup_type,
+a.lookup_code,
+a.enabled_flag,
+a.description,
+b.user_concurrent_program_name,
+a.attribute1 as Fail_Flag,
+a.attribute2 as Run_Time_Flag,
+a.attribute4 as Run_Time_Threshold,
+a.attribute7 as On_Hold_Notification_Flag
+from 
+FND_LOOKUP_VALUES_VL a,
+fnd_concurrent_programs_vl b 
+where 
+lookup_type = 'GEFND_JOB_MONITOR_LT' 
+and b.concurrent_program_name = a.lookup_code
+and b.user_concurrent_program_name like '%GEONT EDI 860 Order Update - APL%';
+--and lookup_code = 'GEARCCUPSETPROG';
