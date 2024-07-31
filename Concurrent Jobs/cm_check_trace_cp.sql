ef52cea45b24d5370a0da70444c546c904c7127f
prakashvel-m <123497509+prakashvel-m@users.noreply.github.com>
2024-07-31 16:05:34 +0530

    Update cm_check_trace_cp.sql

--- Concurrent Jobs/cm_check_trace_cp.sql
+++ Concurrent Jobs/cm_check_trace_cp.sql
@@ -1,4 +1,4 @@
 SELECT fcp.user_concurrent_program_name, fu.user_name as SSO,fu.description as USERNAME, fcp.last_update_date,fcp.enable_trace
 FROM fnd_concurrent_programs_vl fcp
 JOIN fnd_user fu ON fcp.last_updated_by = fu.user_id
-WHERE fcp.user_concurrent_program_name in ('Automatic Remittances Creation Program (API)','GEAR APL Receipt Reverse and CYS Table Update Program - APL');
+WHERE fcp.user_concurrent_program_name in ('Program_Name');
