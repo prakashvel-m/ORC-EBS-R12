0ad6968dc2f3cf593fff2471db9a25f5a3d0590d
prakashvel-m <123497509+prakashvel-m@users.noreply.github.com>
2024-07-31 16:16:28 +0530

    Update fndload.sh

--- Migrations/fndload.sh
+++ Migrations/fndload.sh
@@ -1,7 +1,7 @@
 #Concurrent Programs:
 FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afcpprog.lct concurrent_program.ldt PROGRAM APPLICATION_SHORT_NAME="app_short_name" CONCURRENT_PROGRAM_NAME="concurrent_program_name"
 
-FNDLOAD apps/$appspw O Y UPLOAD $FND_TOP/patch/115/import/afcpprog.lct /geabackup/mprakashvel/FILE_MIGRATION/GEOZF_ACCR_LUMPSUM_TIERS_CCP.ldt CUSTOM_MODE=FORCE
+FNDLOAD apps/$appspw O Y UPLOAD $FND_TOP/patch/115/import/afcpprog.lct /backup/mprakashvel/FILE_MIGRATION/concurrent_program.ldt CUSTOM_MODE=FORCE
 
 
 #Request Groups:
@@ -10,9 +10,9 @@ FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afcpreqg.lct request
 #Request Sets:
 FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afcprset.lct request_set.ldt REQUEST_SET APPLICATION_SHORT_NAME="app_short_name" REQUEST_SET_NAME="request_set_name"
 
-FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afcprset.lct request_set_links.ldt FND_REQUEST_SET_LINKS APPLICATION_SHORT_NAME="GEAR" REQUEST_SET_NAME="FNDRSSUB2177"
+FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afcprset.lct request_set_links.ldt FND_REQUEST_SET_LINKS APPLICATION_SHORT_NAME="AR" REQUEST_SET_NAME="FNDRSSUB2177"
 
-FNDLOAD apps/$appspw O Y UPLOAD $FND_TOP/patch/115/import/afcprset.lct /geabackup/mprakashvel/FILE_MIGRATION/GEOZF_WKD_NON_LUMPSUM_ACCRUAL_RQS_LINK.ldt CUSTOM_MODE=FORCE
+FNDLOAD apps/$appspw O Y UPLOAD $FND_TOP/patch/115/import/afcprset.lct /backup/mprakashvel/FILE_MIGRATION/request_set_links.ldt CUSTOM_MODE=FORCE
 
 #Responsibilities:
 FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afscursp.lct responsibility.ldt FND_RESPONSIBILITY RESP_KEY="responsibility_key"
@@ -30,9 +30,9 @@ FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afscursb.lct data_gr
 FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afcpb.lct profile_option.ldt PROFILE PROFILE_NAME="profile_name"
 
 #User Accounts:
-FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afscursp.lct 515124996.ldt FND_USER USER_NAME=515124996
+FNDLOAD apps/$appspw 0 Y DOWNLOAD $FND_TOP/patch/115/import/afscursp.lct user.ldt FND_USER USER_NAME=515124996
 
-FNDLOAD apps/$appspw 0 Y UPLOAD $FND_TOP/patch/115/import/afscursp.lct 515124996.ldt
+FNDLOAD apps/$appspw 0 Y UPLOAD $FND_TOP/patch/115/import/afscursp.lct user.ldt
 
 Replace "$appspw", "app_short_name", "concurrent_program_name", "request_group_name", "request_set_name", "responsibility_key", "menu_name", "function_name", "data_group_name", "profile_name", and "user_name" with your actual values.
 
