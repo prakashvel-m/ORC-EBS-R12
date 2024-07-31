06e1745a173215d7d62f387c4cd48095baf55e0d
prakashvel-m <123497509+prakashvel-m@users.noreply.github.com>
2024-07-31 19:07:52 +0530

    Update cm_sqlid_details_cp.sql

--- Concurrent Jobs/cm_sqlid_details_cp.sql
+++ Concurrent Jobs/cm_sqlid_details_cp.sql
@@ -1,4 +1,4 @@
-program ,fcr.phase_code,fcr.status_code 
+select b.inst_id,a.request_id ,fcr.requestor,fcr.actual_start_Date,a.os_process_id,b.sid,b.sql_id,b.event,fcr.program ,fcr.phase_code,fcr.status_code 
 from apps.fnd_concurrent_requests a,gv$session b ,apps.fnd_conc_req_summary_v fcr where
 b.audsid(+)= a.oracle_session_id and sid in (select sid 
 from gv$session where sql_id  in ('4zj353uc96yvw','fyj4hqs5zg5wx','fyj4hqs5zg5wx') ) and a.request_id=fcr.request_id;
